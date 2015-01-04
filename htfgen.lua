4a712e649062e25b17b6d7b0fc3398b47763c74f
Michal Hoftich <michal.h21@gmail.com>
2015-01-05 00:07:13 +0100

    initial commit

---
+++ htfgen.lua
@@ -0,0 +1,38 @@
+#!/usr/bin/env texlua
+function file_exists(name)
+  local f=io.open(name,"r")
+  if f~=nil then io.close(f) return true else return false end
+end
+
+local fontname = arg[1]
+if not fontname then 
+  print [[
+  use tfmgen fontname
+  ]]
+  os.exit(1)
+end
+
+local template = [[
+\documentclass{article}
+\usepackage[xhtml,ShowFont]{tex4ht}
+\begin{document}
+\font\x={font}
+\ShowFont\x
+\end{document}
+]]
+
+template = template:gsub("{font}",fontname)
+local tex = io.popen("latex -jobname="..fontname.." -interaction=batchmode", "w")
+tex:write(template)
+tex:close()
+if not file_exists(fontname..".dvi") then
+  print("Fatal error: LaTeX run failed")
+  os.exit(1)
+end
+
+os.execute("tex4ht "..fontname)
+local html_file = io.open(fontname ..".html","r")
+local html = html_file:read("*all")
+html_file:close()
+print(html)
+

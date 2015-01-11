f2c1cf641ba63b6a1e9c9204a987b9acd4876a73
Michal Hoftich <michal.h21@gmail.com>
2015-01-11 20:55:36 +0100

    list encodings

--- lsenc.lua
+++ lsenc.lua
@@ -1,16 +1,26 @@
 kpse.set_program_name("luatex")
 local parsepl = require "parsepl"
+local pl_loader = require "pl_loader"
 
-pl_loader.load("ntxmia")
-local f = io.open("ntxmia.pl","r")
+local s = pl_loader.load("ntxmia")
+--local f = io.open("ntxmia.pl","r")
 
-local s = f:read("*all")
-f:close()
+--local s = f:read("*all")
+--f:close()
 
-local t = parsepl.parse(s, parsepl.actions)
+local find_enc = function(pl)
+  for k,v in ipairs(pl) do
+    if v.type == "encoding" then
+      return v.value
+    end
+  end
+end
+local t = parsepl.parse(s)
 for k,v in ipairs(t) do
   if v.type=="mapfont" then
     local j = pl_loader.load(v.name)
-    print(v.name,string.len(j))
+    print(v.name, j:len())
+    local pl = parsepl.parse(j)
+    print(v.name,find_enc(pl))
   end
 end

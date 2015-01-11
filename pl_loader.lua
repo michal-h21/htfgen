24ce8e6c08f1d3d6e5e7414c8e827c408165a280
Michal Hoftich <michal.h21@gmail.com>
2015-01-11 20:36:48 +0100

    load pl file

---
+++ pl_loader.lua
@@ -0,0 +1,26 @@
+local pl_loader = {}
+
+local load_plist = function(prg,name)
+  local command = prg .." "..name
+  local prg = io.popen(command, "r")
+  local result = prg:read("*all")
+  prg:close()
+  return result
+end
+
+
+pl_loader.load = function(name)
+  local name = name or ""
+  local filename = kpse.find_file(name,"vf")
+  if not filename then
+    filename = kpse.find_file(name,"tfm")
+    if not filename then
+      return nil, "pl_loader: cannot find font file "..name
+    end
+    return load_plist("tftopl", filename) 
+  end
+  return load_plist("vftovp", filename)
+end
+
+
+return pl_loader

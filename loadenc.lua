36260787ea66917279a4a651354c5a3e6d090b53
Michal Hoftich <michal.h21@gmail.com>
2015-01-12 14:47:05 +0100

    fixed enc loading

--- loadenc.lua
+++ loadenc.lua
@@ -2,7 +2,7 @@ kpse.set_program_name("luatex")
 local loadenc = {}
 
 loadenc.load = function(enc)
-  local filename = kpse.find_file(enc,"enc files")
+  local filename = kpse.find_file(enc,"enc files") or enc
   print(enc,filename)
 	if not filename then return nil, "cannot load end ".. enc end
   local f = io.open(filename,"r")
@@ -14,6 +14,7 @@ loadenc.load = function(enc)
 end
 
 loadenc.parse = function(s)
+	local s = s or ""
   local t = {}
   local i = 0
   s:gsub("/([%a%.]+)", function(a)

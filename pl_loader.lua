da8fe30674ed930978a2cf6fe7515311c6862bb6
Michal Hoftich <michal.h21@gmail.com>
2015-01-16 15:47:20 +0100

    standalone function for tfm files

--- pl_loader.lua
+++ pl_loader.lua
@@ -13,14 +13,17 @@ pl_loader.load = function(name)
   local name = name or ""
   local filename = kpse.find_file(name,"vf")
   if not filename then
-    filename = kpse.find_file(name,"tfm")
-    if not filename then
-      return nil, "pl_loader: cannot find font file "..name
-    end
-    return load_plist("tftopl", filename) 
+		return pl_loader.load_tfm(name)
   end
   return load_plist("vftovp", filename)
 end
 
+pl_loader.load_tfm = function(name)
+	local filename = kpse.find_file(name,"tfm")
+	if not filename then
+		return nil, "pl_loader: cannot find font file "..name
+	end
+	return load_plist("tftopl", filename) 
+end
 
 return pl_loader

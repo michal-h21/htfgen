86e3c772fcc7fc8558739b8fb22223abede22ed6
Michal Hoftich <michal.h21@gmail.com>
2015-01-20 17:52:23 +0100

    find encodings in pdftex.map file

--- loadenc.lua
+++ loadenc.lua
@@ -1,6 +1,6 @@
 kpse.set_program_name("luatex")
 local loadenc = {}
-
+local map 
 loadenc.load = function(enc)
   local filename = kpse.find_file(enc,"enc files") or enc
   print(enc,filename)
@@ -22,6 +22,27 @@ loadenc.parse = function(s)
     i = i + 1
   end)
   return t
+end
+
+local load_map = function()
+	local mapfile = kpse.find_file("pdftex","map")
+	print("mapfile",mapfile)
+	if not mapfile then return nil, "cannot find pdftex.map" end
+	--local mapfile = io.open(mapfile,"r")
+	local t = {}
+	for line in io.lines(mapfile) do
+		t[#t+1] = line
+	end
+	map = table.concat(t,"|") 
+	return map
+end
+
+loadenc.find = function(name)
+	local map = map or load_map() or ""
+	local line = map:match("|"..name .. "(.-)|")
+	if not line then return nil, "cannot find font "..name end
+	local enc =  line:match("<%s*(.-)%.enc")
+	return enc
 end
 
 

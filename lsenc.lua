62cabe47f9cfb8ac83983c43d99743234d75c386
Michal Hoftich <michal.h21@gmail.com>
2015-01-27 16:17:05 +0100

    recursively list all virtual fonts

--- lsenc.lua
+++ lsenc.lua
@@ -4,7 +4,7 @@ local pl_loader = require "pl_loader"
 
 
 local name = arg[1] or "ntxmia"
-local s = pl_loader.load(name)
+local s, fonttype = pl_loader.load(name)
 --local f = io.open("ntxmia.pl","r")
 
 --local s = f:read("*all")
@@ -18,16 +18,27 @@ local find_enc = function(pl)
   end
 end
 local t = parsepl.parse(s)
-local print_enc = function(font,enc)
-  print(font.."="..enc)
+local print_enc = function(font,enc,fonttype,indent)
+	local indent = string.rep("  ",indent)
+  print(indent .. font, fonttype, enc)
 end
 
-print_enc(name,find_enc(t))
-for k,v in ipairs(t) do
-  if v.type=="mapfont" then
-    local j = pl_loader.load(v.name)
-    --print(v.name, j:len())
-    local pl = parsepl.parse(j)
-    print_enc(v.name,find_enc(pl))
-  end
+local function list_fonts(t, indent)
+	for k,v in ipairs(t) do
+		if v.type=="mapfont" then
+			local j,fonttype = pl_loader.load(v.name)
+			--print(v.name, j:len())
+			local pl = parsepl.parse(j)
+			print_enc(v.name,find_enc(pl),fonttype,indent)
+			if fonttype == "vf" then
+				list_fonts(pl,indent+1)
+			end
+		end
+	end
+end
+
+print_enc(name,find_enc(t),fonttype,0)
+
+if fonttype == "vf" then
+	list_fonts(t,1)
 end

6249f83767dbb6e96a04b0640206b49075d72091
Michal Hoftich <michal.h21@gmail.com>
2015-06-12 13:45:07 +0200

    Added literate container, fixed table generation

--- htflib.lua
+++ htflib.lua
@@ -15,17 +15,26 @@ function M.make_htf(encoding)
       max = math.max(k, max or k)
       local char_parts = string.explode(g," ")
       local hexs = string.format("'&#x%s;'", table.concat(char_parts,";&#x"))
-      table.insert(htf_table,{k,v, hexs})
+      htf_table[k] = {k,v, hexs}
     elseif v ~= ".notdef" then
-      print("missing",k,v)
+      -- print("missing",k,v)
       missing_glyphs[v] = encoding
-      table.insert(htf_table,{k,v})
+      htf_table[k] = {k,v}
     else
-      table.insert(htf_table,{k,v})
+      htf_table[k] = {k,v}
     end
   end
-  -- make checksum of the encoding
-  return htf_table,missing_glyphs, min, max
+  return htf_table,min, max,missing_glyphs 
 end
 
+-- make containter for the htf font in literate programming format
+
+function M.htf_container(name, path, content)
+  local t ={}
+  t[#t+1] = string.format('\\<%s\\><<<', name)
+  t[#t+1] = content
+  t[#t+1] = '>>>'
+  t[#t+1] = string.format('\\AddFont{%s}{%s%s}{}',name,path,name)
+  return table.concat(t,"\n")
+end
 return M

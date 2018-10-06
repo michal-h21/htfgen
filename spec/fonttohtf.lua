50c4f1bd9c46a94fd0e933b5e69080b63b08a106
Michal Hoftich <michal.h21@gmail.com>
2018-10-06 20:50:55 +0200

    use pdftex.map

--- spec/fonttohtf.lua
+++ spec/fonttohtf.lua
@@ -4,12 +4,15 @@ kpse.set_program_name "luatex"
 local fontobj = require "htflibs.fontobj"
 
 describe("Fontobj test", function()
-  local fonts = fontobj("libertine")
+  -- local fonts = fontobj("libertine")
+  local fonts = fontobj("pdftex")
+
   fonts.maxvf = 10
   it("Should load the map file", function()
     assert.is_table(fonts.map)
   end)
-  fonts:load_virtual_font(kpse.expand_var("$TEXMFDIST") .. "/fonts/vf/public/libertine/LinLibertineT-tlf-sc-t1.vf", "LinLibertineT-tlf-sc-t1")
+  fonts:load_virtual_font(kpse.expand_var("$TEXMFDIST") .. "/fonts/vf/public/libertine/LinLibertineT-tlf-sc-t1.vf")
+  -- fonts:load_virtual_font(kpse.expand_var("$TEXMFDIST") .. "/fonts/vf/public/newtx/ntxmia.vf", "ntxmia")
   -- fonts:load_virtual_fonts("public/libertine")
   -- fonts:load_font("LinLibertineO-tlf-t1")
 end)

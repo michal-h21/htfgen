b75d782ed6344193471b35d14e254b0031276691
Michal Hoftich <michal.h21@gmail.com>
2018-10-07 23:12:16 +0200

    make entity for uni glyhphs too

--- htflibs/glyphload.lua
+++ htflibs/glyphload.lua
@@ -78,7 +78,7 @@ local parse_glyphlist = function()
 		if y then return y end
     -- glyph name may be actually unicode value
 		local c =  x:match("u[n]?[i]?([A-Fa-f0-9]+)$")
-    if c then return c end
+    if c then return make_entity(c) end
     -- test only part of glyph before "." -- disregard the modifier after it
     local basepart = x:match("^([^%.]+)") or ""
     return self[basepart]

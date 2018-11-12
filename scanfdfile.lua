940ab79508966b7e089384bb79235a68020bffa6
Michal Hoftich <michal.h21@gmail.com>
2018-11-12 10:20:47 +0100

    Test if font has been already used

--- scanfdfile.lua
+++ scanfdfile.lua
@@ -1,9 +1,10 @@
+local used = {}
 for line in io.lines() do
   -- local fontname = line:match("%s*%<[^%]+%>%s+[^%s]+%s+([^%s]+)")
   local fontname = line:match("%s*%<[^%]]+%>%s+[^%s]+%s+([^%s]+)")
   -- don't print font substitutions
-  if fontname and not fontname:match("[%*%/]") then
+  if fontname and not fontname:match("[%*%/]") and not used[fontname] then
     print(fontname)
+    used[fontname] = true
   end
-
 end

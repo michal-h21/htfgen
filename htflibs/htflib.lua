2ea7c1a85f612bfb7c8f1bc98ead3145fa5e9178
Michal Hoftich <michal.h21@gmail.com>
2022-01-21 13:12:56 +0100

    add copyright notice to the generated htf files

--- htflibs/htflib.lua
+++ htflibs/htflib.lua
@@ -40,6 +40,52 @@ function M.htf_container(name, path, content)
 end
 
 
+function M.add_copyright(name, htf_table)
+  -- add copyright text to the htf_table
+  local name = name or ""
+  local license = {
+    string.format("%% generated from %s.tfm, %s", name, os.date("%Y-%m-%d-%H:%M")),
+    string.format("%% Copyright %s TeX Users Group", os.date("%Y")),
+    "%",
+    "% This work may be distributed and/or modified under the",
+    "% conditions of the LaTeX Project Public License, either",
+    "% version 1.3c of this license or (at your option) any",
+    "% later version. The latest version of this license is in",
+    "%   http://www.latex-project.org/lppl.txt",
+    "% and version 1.3c or later is part of all distributions",
+    "% of LaTeX version 2005/12/01 or later.",
+    "%",
+    "% This work has the LPPL maintenance status \"maintained\".",
+    "%",
+    "% The Current Maintainer of this work",
+    "% is the TeX4ht Project <http://tug.org/tex4ht>.",
+    "%",
+    "% If you modify this program, changing the",
+    "% version identification would be appreciated."
+  }
+  if not htf_table or type(htf_table) ~= "table" then return htf_table end
+
+  -- find longest string in the htf_table, so we can nicely format the license
+  local max = math.max
+  local len = string.len
+  local rep = string.rep
+  local max_val = 0
+  for i = 2, #license+1 do
+    max_val = max(max_val, len(htf_table[i]))
+  end
+  for i, text in ipairs(license) do
+    -- we don't want to add copyright to the first line of the htf_table, so we must use an offset
+    local ni = i + 1
+    if htf_table[ni] then
+      local old_text = htf_table[ni]
+      -- we want to format copyright nicely, so we need to add some spacing
+      local spacing  = rep(" ",max_val - len(old_text) + 2)
+      htf_table[ni] =  old_text .. spacing .. text
+    end
+  end
+  return htf_table
+end
+
 function M.htf_table(name, htf_table, min,max)
   local t = {}
   t[#t+1] = string.format("%s %i %i",name,min,max)
@@ -55,8 +101,10 @@ function M.htf_table(name, htf_table, min,max)
     t[#t+1] = string.format(format,hex, name, count)
   end
   t[#t+1] = string.format("%s %i %i",name,min,max)
+  t = M.add_copyright(name, t)
   return table.concat(t,"\n")
 end
+
 
 -- convert the font object to the htf table
 function M.fontobj_to_htf_table(fontobj)
